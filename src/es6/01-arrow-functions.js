6e3198759f9ed633771f55074f340a3b0dc54206
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-21 18:34:39 -0300

    ES6: let y const, y arrow functions

---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,14 @@
+// ******************** ARROW FUNCTIONS (Funciones tipo flecha) ******************** //
+
+// ? Before ES6
+function square(num) {
+  return num*num;
+}
+
+// ? After ES6
+  // with code block
+const square1 = (num) => {
+  return num*num;
+}
+  // with implicit return
+const square2 = num => num*num;

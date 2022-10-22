930b9c2277ea98be261ee91778f91ecf686ba25a
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-22 03:35:04 -0300

    ES6: spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -10,3 +10,29 @@ console.log(a, fruits[1]);
 let user = { username: 'Oscar', age: 34 };
 let { username, age } = user;
 console.log(username, age);
+
+// *********************************** SPREAD OPERATOR *********************************** //
+
+let person = {
+  name: 'Rahydee',
+  age: 35
+}
+let country = 'CL';
+
+let data = {
+  id: 1,
+  ...person,
+  country
+}
+console.log(data);
+
+// **************************************** REST **************************************** //
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4);

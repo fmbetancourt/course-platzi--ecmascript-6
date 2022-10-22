ce0c92dafffe4e04ea651be1d4b866e94baae6ed
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-22 03:16:58 -0300

    ES6: asignación de desestructuración

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,12 @@
+// *********************************** REST SPREADS *********************************** //
+
+// Arrays destructuring
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a, b);
+console.log(a, fruits[1]);
+
+// Object destructuring
+let user = { username: 'Oscar', age: 34 };
+let { username, age } = user;
+console.log(username, age);

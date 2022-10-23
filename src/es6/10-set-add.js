1bc7f5a8ddc3f391d725fb930906b42f1805cb8b
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-23 15:32:30 -0300

    ES6: set-add

---
+++ src/es6/10-set-add.js
@@ -0,0 +1,8 @@
+// *********************************** SET ADD *********************************** //
+
+const list = new Set();
+
+list.add('item 1');
+list.add('item 2').add('item 3');
+
+console.log(list);

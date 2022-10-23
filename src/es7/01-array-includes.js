187c80a7339f15d793f780b90595b8635ce12657
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-23 15:51:33 -0300

    ES7: exponentiation operator y array includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,9 @@
+// *********************************** ARRAY INCLUDES *********************************** //
+
+let numbers = [1,2,3,4,5,6,7,8];
+console.log(numbers.includes(4));
+console.log(numbers.includes(9));
+
+let names = ['Freddy', 'Rahydee', 'Lucia', 'Mildred'];
+console.log(names.includes('Freddy'));
+console.log(names.includes('freddy'));

579005274fbd70a036aa3e5fea0efe5e20b2b4ff
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-25 14:11:41 -0300

    ES8: object entries y object values

---
+++ src/es8/01-object-values.js
@@ -0,0 +1,11 @@
+// *********************************** OBJECT VALUES *********************************** //
+
+const countries = {
+  VE: 'Venezuela',
+  MX: 'Mexico',
+  CO: 'Colombia',
+  CL: 'Chile',
+  PE: 'Perú',
+}
+
+console.log(Object.values(countries));

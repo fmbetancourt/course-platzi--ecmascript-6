74cbdf51e24fbe3f096cce9eb1bb085247db1cba
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-25 14:28:51 -0300

    ES8: string padding y trailing commas

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,5 @@
+// *********************************** STRING PADDING *********************************** //
+
+const string = 'Hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(7, 'hi'));

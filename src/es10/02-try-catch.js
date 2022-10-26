5714da83724ad2693911dbf8247ce22c0846eea2
Freddy Moreno <freddy.moreno@gmail.com>
2022-10-26 13:29:02 -0300

    ES10: try catch y fromEntries

---
+++ src/es10/02-try-catch.js
@@ -0,0 +1,13 @@
+// *********************************** TRY CATCH *********************************** //
+
+try {
+  hello();
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  anotherFn();
+} catch {
+  console.log('Esto es un error');
+}
---
+++ src/es10/03-formentries.js
@@ -0,0 +1,5 @@
+// *********************************** FORMENTRIES *********************************** //
+
+const entries  = new Map([['name','freddy'],['age',34]]);
+console.log(entries);
+console.log(Object.fromEntries(entries));

c24075bfc559c1464d1d4957685b36f4a598a9ae
Freddy J Moreno B <freddy.moreno@gmail.com>
2022-11-04 16:22:32 -0300

    ES11: BigInt y Nullish

---
+++ src/es11/02-nullish.js
@@ -0,0 +1,14 @@
+// *********************************** NULLISH *********************************** //
+
+const anotherNumber1 = 1;
+const validate1 = anotherNumber1 ?? 5;
+console.log(validate1);
+
+const anotherNumber2 = null;
+const validate2 = anotherNumber2 ?? 5;
+console.log(validate2);
+
+const anotherNumber3 = undefined;
+const validate3 = anotherNumber3 ?? 5;
+console.log(validate3);
+

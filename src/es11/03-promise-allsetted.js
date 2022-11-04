525aad1b842dea3b5a183cad772a381aa1fc39e7
Freddy J Moreno B <freddy.moreno@gmail.com>
2022-11-04 16:45:01 -0300

    ES11: Promise.allSettled

---
+++ src/es11/03-promise-allsetted.js
@@ -0,0 +1,11 @@
+// *********************************** PROMISE ALLSETTED *********************************** //
+
+const promise1 = new Promise((resolve, reject) => reject("Reject 1"));
+const promise2 = new Promise((resolve, reject) => resolve("Resolve 1"));
+const promise3 = new Promise((resolve, reject) => resolve("Resolve 2"));
+
+Promise.allSettled([promise1, promise2, promise3])
+  .then(response => console.log(response));
+
+Promise.allSettled([promise1, promise2, promise3])
+  .then(response => console.table(response));
